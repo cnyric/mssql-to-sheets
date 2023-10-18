b23beceaae7fdfad775ce024d87001deac7496c8
Daniel Sieradski <dsieradski@cnyric.org>
2023-10-17 22:56:10 -0400

    mmk

--- src/lib/service/logger.ts
+++ src/lib/service/logger.ts
@@ -3,17 +3,15 @@ import status from 'http-status';
 import { log } from '../util.js';
 
 function logger(req: any, res: any, next: any) {
-  const { info, warn, error } = log;
-
   res.on('finish', () => {
     const message = [req.method, decodeURI(<string>req.url), res.statusCode, status[res.statusCode]];
 
     if (res.statusCode < 400) {
-      info(...message);
+      log.info(...message);
     } else if (res.statusCode < 500) {
-      warn(...message);
+      log.warn(...message);
     } else {
-      error(...message);
+      log.error(...message);
     }
   });
 
