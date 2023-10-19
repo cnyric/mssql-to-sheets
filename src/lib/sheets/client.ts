17e97678c264cd5dc3d41e1ddace21916aff2fb3
Daniel Sieradski <dsieradski@cnyric.org>
2023-10-19 09:29:18 -0400

    google creds

--- src/lib/sheets/client.ts
+++ src/lib/sheets/client.ts
@@ -2,16 +2,20 @@ import { google } from 'googleapis';
 import { JWT } from 'google-auth-library';
 import { readFile } from 'fs/promises';
 
-const key = JSON.parse(await readFile(`${process.cwd()}/credentials.json`, 'utf-8'));
+const scopes = [
+  'https://www.googleapis.com/auth/spreadsheets',
+  'https://www.googleapis.com/auth/drive',
+  'https://www.googleapis.com/auth/drive.file'
+];
+
+const key = JSON.parse(
+  await readFile(process.env.GOOGLE_APPLICATION_CREDENTIALS ?? `${process.cwd()}/credentials.json`, 'utf-8')
+);
 
 const auth = new JWT({
   email: key.client_email,
   key: key.private_key,
-  scopes: [
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive',
-    'https://www.googleapis.com/auth/drive.file'
-  ]
+  scopes
 });
 
 const sheets = google.sheets({ version: 'v4', auth });
