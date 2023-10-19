bf390925a27dae44ed364a89ad80201264d9ad4d
Daniel Sieradski <dsieradski@cnyric.org>
2023-10-19 08:58:54 -0400

    add test workflow

---
+++ tests/test.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE test;
+
+USE test;
+
+CREATE TABLE test (
+  id INT PRIMARY KEY,
+  name VARCHAR(50),
+  age INT,
+  email VARCHAR(100),
+  is_active BIT,
+  created_at DATETIME
+);
+
+INSERT INTO test (id, name, age, email, is_active, created_at)
+VALUES
+  (1, 'Alice', 30, 'alice@example.com', 1, '2022-01-01 00:00:00'),
+  (2, 'Bob', 40, 'bob@example.com', 0, '2022-01-02 00:00:00'),
+  (3, 'Charlie', 50, 'charlie@example.com', 1, '2022-01-03 00:00:00');
