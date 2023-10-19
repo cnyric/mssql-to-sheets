8a22e0e0ac9b53ce3d2318a57d874c18a5d0dd1f
Daniel Sieradski <dsieradski@cnyric.org>
2023-10-19 10:04:56 -0400

    and again

--- tests/test.sql
+++ tests/test.sql
@@ -1,7 +1,4 @@
-CREATE DATABASE testDb;
-GO;
-
-USE testDb;
+USE tempdb;
 
 CREATE TABLE testTable (
   id INT PRIMARY KEY,
@@ -12,7 +9,7 @@ CREATE TABLE testTable (
   created_at DATETIME
 );
 
-INSERT INTO test (id, name, age, email, is_active, created_at)
+INSERT INTO testTable (id, name, age, email, is_active, created_at)
 VALUES
   (1, 'Alice', 30, 'alice@example.com', 1, '2022-01-01 00:00:00'),
   (2, 'Bob', 40, 'bob@example.com', 0, '2022-01-02 00:00:00'),
